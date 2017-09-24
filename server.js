916477cd58ecfcf260936dde19003203d439c54e
Andi Andreas <nexuister@gmail.com>
2017-09-24 00:11:10 -0400

    Added a super basic server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+const indexPage = fs.readFileSync("index.html", "utf8");
+
+app.use(express.static("."));
+app.listen(8080, () => console.log("Ready!"));
