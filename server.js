194989a995f62471fbc87797f2d4c18d0a39d4a7
Andi Andreas <nexuister@gmail.com>
2017-10-13 16:34:55 -0400

    Add watchers for HTML and CSS files so they are automatically transpiled

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const fs = require("fs");
-const pug = require("pug");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send(pug.renderFile("index.pug"));
-});
 app.use(express.static("."));
 
 app.listen(8080, () => {
