8b27ced1155a2b164939d452fc96433e1deab67d
Andi Andreas <nexuister@gmail.com>
2017-10-27 17:42:34 -0400

    Begin to add web hook update support for server

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+const { spawn } = require("child_process");
+const fs = require("fs");
 const express = require("express");
 const server = express();
 
+// Make sure dist exists
+if (!fs.existsSync("dist")) {
+    console.log("Dist does not exist to serve from!");
+    process.exit(1); // Force quit
+}
+// Serve directly from the dist folder
 server.use(express.static("dist"));
-server.listen(8080, () => console.log("Online"));
+
+server.get("/webhook/push", (req, res) => {
+    res.send("Success");
+    console.log("Starting push update!");
+    let updateProcess = spawn("/bin/sh", ["-c", "git pull && npm run build"]);
+    updateProcess.stdout.on("data", (data) => {
+        console.log(data.toString());
+    });
+    updateProcess.on("error", (err) => console.error(err));
+    updateProcess.on("exit", (code) => console.log(`Finished updating with code ${code}`));
+});
+
+server.listen(8080, () => console.log("Online!"));
