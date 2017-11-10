92341bbc9b0e10754857e53beb37d454edcc9e3c
Andi Andreas <nexuister@gmail.com>
2017-11-09 22:42:42 -0500

    Try to use CSS grid for navbar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,17 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../pages/Home.vue";
 import Projects from "../pages/Projects.vue";
+import People from "../pages/People.vue";
+import Workshops from "../pages/Workshops.vue";
 import App from "../App.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/projects', component: Projects }
+  { path: '/projects', component: Projects },
+  { path: '/people', component: People },
+  { path: '/workshops', component: Workshops }
 ]
 
 const router = new VueRouter({
