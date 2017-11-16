6d63a76e4a06bb49e357c14f81ef3b2918ccce3d
Andi Andreas <nexuister@gmail.com>
2017-11-16 17:56:43 -0500

    Fix issue with webpack not loading fonts correctly

--- src/App.vue
+++ src/App.vue
@@ -8,33 +8,39 @@
 </template>
 
 <script>
-  import Navbar from './components/Navbar.vue'
+import Navbar from "./components/Navbar.vue";
 
-  export default {
-    name: 'app',
-    components: { Navbar }
-  }
+export default {
+  name: "app",
+  components: { Navbar }
+};
 </script>
 
 <style lang="scss">
-  body {
-    background: #f2f3f7;
-    font-family: "Moderat";
-    font-size: 16px;
-    line-height: 20px;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-  }
+@import "styles/vars";
+@import "styles/fonts";
 
-  .page {
-    padding: 20px;
-  }
 
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .5s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-    position: absolute; /* If this page doesn't keep its place, the new one will appear below it before it's fully faded out */
-  }
+body {
+  background: #f2f3f7;
+  font-family: "Moderat";
+  font-size: 16px;
+  line-height: 20px;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+
+.page {
+  padding: 20px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  position: absolute; /* If this page doesn't keep its place, the new one will appear below it before it's fully faded out */
+}
 </style>
