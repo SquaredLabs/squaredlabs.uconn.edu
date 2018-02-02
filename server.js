f9a94039bdcd77f1e8808baea40e92ba3e11021e
Andi Andreas <nexuister@gmail.com>
2018-02-02 13:01:16 -0500

    Add favicon and modify webpack config to serve favicons (fixes #19)

--- server.js
+++ server.js
@@ -11,15 +11,4 @@ if (!fs.existsSync("dist")) {
 // Serve directly from the dist folder
 server.use(express.static("dist"));
 
-server.get("/webhook/push", (req, res) => {
-    res.send("Success");
-    console.log("Starting push update!");
-    let updateProcess = spawn("/bin/sh", ["-c", "git pull && npm run build"]);
-    updateProcess.stdout.on("data", (data) => {
-        console.log(data.toString());
-    });
-    updateProcess.on("error", (err) => console.error(err));
-    updateProcess.on("exit", (code) => console.log(`Finished updating with code ${code}`));
-});
-
 server.listen(8080, () => console.log("Online!"));
