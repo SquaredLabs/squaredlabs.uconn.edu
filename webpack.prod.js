2cfa3541630fc8eb2a5d24c046cbaace80a8712a
Brandon Cheng <gluxon@users.noreply.github.com>
2018-02-18 21:03:22 -0500

    Favor webpack -p flag over babel minify plugin
    The babel-minify-webpack-plugin was giving me a strange error when
    building. The Webpack builtin production minifier f does not seem have
    this issue.
    
    https://github.com/webpack/webpack-sources/issues/28

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
@@ -11,10 +10,6 @@ module.exports = merge(common, {
             'process.env': {
               NODE_ENV: '"production"' // Why double quotes? https://github.com/thgh/vuejs-templates-rollup/issues/1#issuecomment-255096585
             }
-        }),
-        new MinifyPlugin({}, 
-        {
-            comments: false
         }),
         new HtmlWebpackPlugin({
             template: "src/images/favicon/favicon.ico",
