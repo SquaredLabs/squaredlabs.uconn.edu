a36acc6f11896ba03bdbad8391013800a74d7b94
Andi Andreas <nexuister@gmail.com>
2018-02-26 15:35:35 -0500

    Add mini person cards and project cards to Projects page

---
+++ src/components/MiniPersonCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div style="position: relative;">
+        <img class="mini__icon" :src="icon" />
+        <div class="mini__card">
+            <b>Raymond Baldwin</b>
+            <p style="margin: 0">Project Lead</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["icon", "name", "position"]
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "../styles/vars";
+
+.mini__icon {
+    width: 80px;
+    height: 80px;
+}
+
+.mini__card {
+    background: white;
+    padding: 10px;
+    position: absolute;
+    top: 22px;
+    bottom: 10px;
+    left: 40px;
+    white-space: nowrap;
+}
+
+</style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,22 +1,31 @@
 <template>
-  <!-- <section>
-    <construction msg="PROJECTS" height="100vh" />
-  </section> -->
   <div>
     <section>
       <img src="https://picsum.photos/1920/445" style="width: calc(100% + 40px); height:440px; margin-left: -20px;" />
-    </section>
-    <section style="margin-top: -300px; margin-bottom: 300px">
-      <quad-layout text="projects">
-        <div class="quad__col" style="margin-top: -80px">
-          <div style="background: white; padding: 20px">
+      <quad-layout text="projects" style="margin-top: -300px">
+        <div class="quad__col">
+          <div style="background: white; padding: 20px; margin-top: -80px">
             <p>We build websites that support UConn research.  All of our projects are presently conceived of internally.  Eventually we’d love to get to a point where we have the bandwidth to take on externally proposed projects.</p>
           </div>
+          <div style="margin-top: 200px">
+            <b>Hovered Project Name</b>
+            <p>Client Name</p>
+            <p>January - August 2017</p>
+            <mini-person-card v-for="x in 5" :key="x" name="Raymond Baldwin" icon="https://picsum.photos/80/80" position="Project Manager" />
+          </div>
         </div>
-        <div class="quad__col">
-          <project-card class="quad__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
-            <p>A short, fun description.</p>
-          </project-card>
+        <div class="quad__triple_col" style="margin-top: 30px">
+          <triple-layout v-for="x in 3" :key="x" style="padding-bottom: 20px">
+            <project-card class="triple__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
+              <p>A short, fun description.</p>
+            </project-card>
+            <project-card class="triple__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
+              <p>A short, fun description.</p>
+            </project-card>
+            <project-card class="triple__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
+              <p>A short, fun description.</p>
+            </project-card>
+          </triple-layout>
         </div>  
       </quad-layout>
       </section>
@@ -26,13 +35,17 @@
 <script>
 import Construction from "../components/Construction.vue";
 import QuadLayout from "../components/QuadLayout.vue";
+import TripleLayout from "../components/TripleLayout.vue";
 import ProjectCard from "../components/ProjectCard.vue";
+import MiniPersonCard from "../components/MiniPersonCard.vue";
 
 export default {
   components: {
     Construction,
     QuadLayout,
-    ProjectCard
+    TripleLayout,
+    ProjectCard,
+    MiniPersonCard
   }
 };
 </script>
