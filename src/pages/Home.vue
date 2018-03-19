d5e27205a4a5ca7bca4e9f449a2206bc326f9b1a
Andi Andreas <nexuister@gmail.com>
2018-03-18 23:27:53 -0400

    Finish projects page using new Layout component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,84 +1,80 @@
 <template>
-    <div>
-        <section>
-            <construction msg="This space will have something really cool here eventually!" height="80vh" />
-        </section>
-        <section>
-          <quad-layout style="margin-top:80px" text="projects" padding-top="110px">
-              <div class="quad__col" style="position: relative; top: -80px">
-                  <p>Welcome to ⬚² [squared] labs. Together with UConn’s very best <router-link class="link--highlighted" to="/people" >students</router-link>, we build cutting-edge <router-link class="link--highlighted" to="/projects" >websites</router-link> that support UConn’s world-class research infrastructure.</p>
-                  <p>View our history, purpose, and space on the <router-link class="link--highlighted" to="/lab" >lab page</router-link>.</p>
-              </div>
-              <project-card class="quad__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
-                <p>A short, fun description.</p>
-              </project-card>
-              <project-card class="quad__col" name="Lincus" background="../images/lincus.png" link="/projects" />
-              <project-card class="quad__col" name="GrantTrails" background="../images/huskytrails.png" link="/projects" />
-              <p class="quad__bg-text" style="padding-top: 110px">projects</p>
-          </quad-layout>
-          <div class="flex-layout">
-            <router-link class="link--highlighted link--pulled-right" to="/projects">See all our projects →</router-link>
-          </div>
-        </section>
-        <section>
-          <quad-layout text="workshops">
-            <div class="quad__triple_col">
-              <workshop-card
-                name="Workshop Name"
-                time="13:00-17:00" 
-                date="Jan 1, 2018" 
-                addressTop="1 Bishop Circle" 
-                addressBottom="Storrs CT 06269"
-                description="Any other relevant text can go here. Now it’s longer though, which is pretty neat. You can talk about the workshop now."
-                :technologies="['https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png', 'http://ryanchristiani.com/wp-content/uploads/2015/06/js-logo.png']"
-                registration-link="http://google.com"
-              />
-            </div>
-            <div class="quad__col" style="margin-top: 80px">
-              <p>Learning from others and sharing what we know is central to the ⬚² labs philosophy.  We see workshops as an opportunity to do both (in addition to getting to know the UConn web development community).</p>
-              <p>Our workshops are open to all members of the UConn community, for free.</p>
-              <router-link class="link--highlighted" to="/workshops">View all workshops →</router-link>
-            </div>
-            <p class="quad__bg-text" style="padding-top: 310px">workshops</p>
-          </quad-layout>
-        </section>
-        <!-- <section>
+  <div>
+    <section>
+      <construction msg="This space will have something really cool here eventually!" height="80vh" />
+    </section>
+    <section>
+      <quad-layout style="margin-top:80px" text="projects" padding-top="110px">
+        <div class="quad__col" style="position: relative; top: -80px">
+          <p>Welcome to ⬚² [squared] labs. Together with UConn’s very best
+            <router-link class="link--highlighted" to="/people">students</router-link>, we build cutting-edge
+            <router-link class="link--highlighted" to="/projects">websites</router-link> that support UConn’s world-class research infrastructure.</p>
+          <p>View our history, purpose, and space on the
+            <router-link class="link--highlighted" to="/lab">lab page</router-link>.</p>
+        </div>
+        <project-card class="quad__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
+          <p>A short, fun description.</p>
+        </project-card>
+        <project-card class="quad__col" name="Lincus" background="../images/lincus.png" link="/projects" />
+        <project-card class="quad__col" name="GrantTrails" background="../images/huskytrails.png" link="/projects" />
+        <p class="quad__bg-text" style="padding-top: 110px">projects</p>
+      </quad-layout>
+      <div class="flex-layout">
+        <router-link class="link--highlighted link--pulled-right" to="/projects">See all our projects →</router-link>
+      </div>
+    </section>
+    <section>
+      <quad-layout text="workshops">
+        <div class="quad__triple_col">
+          <workshop-card name="Workshop Name" time="13:00-17:00" date="Jan 1, 2018" addressTop="1 Bishop Circle" addressBottom="Storrs CT 06269" description="Any other relevant text can go here. Now it’s longer though, which is pretty neat. You can talk about the workshop now." :technologies="['https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png', 'http://ryanchristiani.com/wp-content/uploads/2015/06/js-logo.png']" registration-link="http://google.com" />
+        </div>
+        <div class="quad__col" style="margin-top: 80px">
+          <p>Learning from others and sharing what we know is central to the ⬚² labs philosophy. We see workshops as an opportunity to do both (in addition to getting to know the UConn web development community).</p>
+          <p>Our workshops are open to all members of the UConn community, for free.</p>
+          <router-link class="link--highlighted" to="/workshops">View all workshops →</router-link>
+        </div>
+        <p class="quad__bg-text" style="padding-top: 310px">workshops</p>
+      </quad-layout>
+    </section>
+    <!-- <section>
           <construction msg="Not sure what we're doing with the blog yet!" height="40vh" style="margin-top: 160px" />
         </section> -->
-         <section style="padding-top: 6em">
-          <div class="flex-layout">
-              <router-link class="link--highlighted link--pulled-right" to="/people">Meet the team →</router-link>
-          </div>
-          <quad-layout>
-              <div class="quad__col" style="position: relative; top: -80px">
-                  <p>⬚² labs team members are a stellar group who share a passion for building beautiful, functional websites.</p>
-                  <p>We are always looking for talented and hard-working students to join our ranks. We look for students with a track record of building cool stuff in their spare time, web experience, and a passion for innovation and creation. If that sounds like you, <router-link class="link--highlighted" to="/connect">get in touch!</router-link></p>
-              </div>
-              <person-card class="quad__col" name="Dan Schwartz" role="Director" background="../images/dan.jpg">
-                <p>“Take life (and everything) with a grain of salt.”</p>
-              </person-card>
-              <person-card class="quad__col" name="Dan Schwartz" role="Director" background="../images/dan.jpg">
-                <p>“Take life (and everything) with a grain of salt.”</p>
-              </person-card>
-              <person-card class="quad__col" name="Dan Schwartz" role="Director" background="../images/dan.jpg">
-                <p>“Take life (and everything) with a grain of salt.”</p>
-              </person-card>
-              <p class="quad__bg-text" style="margin-top: -20px">people</p>
-          </quad-layout>
-        </section>
-         <section>
-          <quad-layout style="margin-top: 260px">
-              <connect-card class="quad__col" link="https://gitlab.com/squared-labs" background="../images/gitlab.svg" />
-              <connect-card class="quad__col" background="../images/mail.svg" hidden-image="../images/contact.png" />
-              <connect-card class="quad__col" link="http://www.foundation.uconn.edu/send-your-gift/" background="../images/btc.svg" />
-               <div class="quad__col" style="position: relative; top: -80px">
-                  <p>If you like what you see here and want to offer support, or get involved as something other than a member of ⬚² labs, we would love to hear from you! From workshops, to open source contributions, to donations, there are plenty ways to become a part of what we do. </p>
-                  <router-link class="link--highlighted" to="/connect">Connect with us →</router-link>
-              </div>
-              <p class="quad__bg-text" style="margin-top: -20px">connect</p>
-          </quad-layout>
-        </section>
-    </div>
+    <section style="padding-top: 6em">
+      <div class="flex-layout">
+        <router-link class="link--highlighted link--pulled-right" to="/people">Meet the team →</router-link>
+      </div>
+      <quad-layout>
+        <div class="quad__col" style="position: relative; top: -80px">
+          <p>⬚² labs team members are a stellar group who share a passion for building beautiful, functional websites.</p>
+          <p>We are always looking for talented and hard-working students to join our ranks. We look for students with a track record of building cool stuff in their spare time, web experience, and a passion for innovation and creation. If that sounds like you,
+            <router-link class="link--highlighted" to="/connect">get in touch!</router-link>
+          </p>
+        </div>
+        <person-card class="quad__col" name="Dan Schwartz" role="Director" background="../images/dan.jpg">
+          <p>“Take life (and everything) with a grain of salt.”</p>
+        </person-card>
+        <person-card class="quad__col" name="Dan Schwartz" role="Director" background="../images/dan.jpg">
+          <p>“Take life (and everything) with a grain of salt.”</p>
+        </person-card>
+        <person-card class="quad__col" name="Dan Schwartz" role="Director" background="../images/dan.jpg">
+          <p>“Take life (and everything) with a grain of salt.”</p>
+        </person-card>
+        <p class="quad__bg-text" style="margin-top: -20px">people</p>
+      </quad-layout>
+    </section>
+    <section>
+      <quad-layout style="margin-top: 260px">
+        <connect-card class="quad__col" link="https://gitlab.com/squared-labs" background="../images/gitlab.svg" />
+        <connect-card class="quad__col" background="../images/mail.svg" hidden-image="../images/contact.png" />
+        <connect-card class="quad__col" link="http://www.foundation.uconn.edu/send-your-gift/" background="../images/btc.svg" />
+        <div class="quad__col" style="position: relative; top: -80px">
+          <p>If you like what you see here and want to offer support, or get involved as something other than a member of ⬚² labs, we would love to hear from you! From workshops, to open source contributions, to donations, there are plenty ways to become a part of what we do. </p>
+          <router-link class="link--highlighted" to="/connect">Connect with us →</router-link>
+        </div>
+        <p class="quad__bg-text" style="margin-top: -20px">connect</p>
+      </quad-layout>
+    </section>
+  </div>
 </template>
 
 <script>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <section id="jumbotron">
-      <img src="https://picsum.photos/1920/445" style="height:440px" />
-      <layout style="margin-top: -300px">
-        <div class="layout__col--quarter">
-          <div style="background: white; padding: 20px; margin-top: -80px" id="white-box">
-            <p>We build websites that support UConn research.  All of our projects are presently conceived of internally.  Eventually we’d love to get to a point where we have the bandwidth to take on externally proposed projects.</p>
+    <section id="jumbotron" style="margin-bottom: 100px">
+      <img class="img--fill-container" src="https://picsum.photos/1920/445" style="height:440px" />
+      <layout style="margin-top: -300px;">
+        <div class="layout__col--quarter marginned--horizontally">
+          <div style="background: white; margin-top: -80px" class="padded" id="white-box">
+            <p>We build websites that support UConn research. All of our projects are presently conceived of internally. Eventually we’d love to get to a point where we have the bandwidth to take on externally proposed projects.</p>
           </div>
           <div style="margin-top: 200px" id="project-details">
             <b>Hovered Project Name</b>
@@ -14,15 +14,15 @@
             <mini-person-card v-for="x in 5" :key="x" name="Raymond Baldwin" icon="https://picsum.photos/80/80" position="Project Manager" />
           </div>
         </div>
-        <div class="layout__col--triple">
+        <div class="layout__col--pacman">
           <layout v-for="x in 3" :key="x" style="padding-bottom: 20px">
-            <project-card class="layout__col--third" name="Innovation Portal" background="../images/ip.png" link="/projects">
+            <project-card class="layout__col--third marginned--horizontally" name="Innovation Portal" background="../images/ip.png" link="/projects">
               <p>A short, fun description.</p>
             </project-card>
-            <project-card class="layout__col--third" name="Innovation Portal" background="../images/ip.png" link="/projects">
+            <project-card class="layout__col--third marginned--horizontally" name="Innovation Portal" background="../images/ip.png" link="/projects">
               <p>A short, fun description.</p>
             </project-card>
-            <project-card class="layout__col--third" name="Innovation Portal" background="../images/ip.png" link="/projects">
+            <project-card class="layout__col--third marginned--horizontally" name="Innovation Portal" background="../images/ip.png" link="/projects">
               <p>A short, fun description.</p>
             </project-card>
           </layout>
@@ -30,115 +30,128 @@
       </layout>
     </section>
 
-
-    <!-- <section>
-      <img src="https://picsum.photos/1920/445" style="width: 100%; height:440px;" />
-      <quad-layout text="projects" style="margin-top: -300px">
-        <div class="quad__col">
-          <div style="background: white; padding: 20px; margin-top: -80px">
-            <p>We build websites that support UConn research.  All of our projects are presently conceived of internally.  Eventually we’d love to get to a point where we have the bandwidth to take on externally proposed projects.</p>
-          </div>
-          <div style="margin-top: 200px">
-            <b>Hovered Project Name</b>
-            <p>Client Name</p>
-            <p>January - August 2017</p>
-            <mini-person-card v-for="x in 5" :key="x" name="Raymond Baldwin" icon="https://picsum.photos/80/80" position="Project Manager" />
-          </div>
-        </div>
-        <div class="quad__triple_col" style="margin-top: 30px">
-          <triple-layout v-for="x in 3" :key="x" style="padding-bottom: 20px">
-            <project-card class="triple__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
-              <p>A short, fun description.</p>
-            </project-card>
-            <project-card class="triple__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
-              <p>A short, fun description.</p>
-            </project-card>
-            <project-card class="triple__col" name="Innovation Portal" background="../images/ip.png" link="/projects">
-              <p>A short, fun description.</p>
-            </project-card>
-          </triple-layout>
-        </div>  
-      </quad-layout>
-    </section> -->
-    <section style="padding-top: 100px">
+    <section id="1">
       <layout>
-        <div class="layout__col--quarter">
+        <div class="layout__col--quarter marginned--horizontally">
           <h1 class="number">01</h1>
         </div>
-        <div class="layout__col--triple">
-          <layout style="padding-bottom: 80px">
-            <div class="layout__col--half">
-              <p>You wouldn’t build a building without architectural drawings.  We don’t build websites without spending a considerable amount of time on design, ensuring that every choice contributes to crafting the most effective solution.</p>
+        <div class="layout__col--pacman marginned--horizontally">
+          <layout id="assess" class="marginned--vertically--6x">
+            <div class="layout__col--half" style="padding-right: 20px">
+              <p>You wouldn’t build a building without architectural drawings. We don’t build websites without spending a considerable amount of time on design, ensuring that every choice contributes to crafting the most effective solution.</p>
             </div>
             <div class="layout__col--half">
-              <img src="https://picsum.photos/360/100" style="padding-bottom: 20px;" />
-              <img src="https://picsum.photos/360/100" />
+              <layout vertical="true">
+                <div class="layout__col--whole marginned--vertically">
+                  <img class="img--fill-container" src="https://picsum.photos/360/100" />
+                </div>
+                <div class="layout__col--whole">
+                  <img class="img--fill-container" src="https://picsum.photos/360/100" />
+                </div>
+              </layout>
             </div>
           </layout>
-          <layout vertical="true" style="padding-bottom: 80px">
+          <layout vertical="true" id="question" class="marginned--vertically--6x">
+            <!-- 25vw to constrain text length -->
             <div class="layout__col--quarter" style="width: 25vw">
               <p>We believe that thoughtfully discussing, debating, and documenting all decisions (sometimes quite heatedly!) early on pays dividends later.</p>
             </div>
             <div class="layout__col--whole">
-              <img style="width: calc(100% + 40px)" src="https://picsum.photos/760/100" />
+              <img class="img--fill-container" style="width: calc(100% + 20px)" src="https://picsum.photos/760/100" />
             </div>
           </layout>
-          <layout style="padding-bottom: 80px">
-            <div class="layout__col--third">
+          <layout id="plan">
+            <div class="layout__col--quarter" style="margin-top: 40px;">
               <p>We lay out every pattern and convention, ensuring continuity throughout the project from the get-go. Color palette, wireframes, stack choice, database architecture, timeline — you name it — it’s vetted for consistency with the overall project goals.</p>
             </div>
-            <div class="layout__col--third">
-              <img src="https://picsum.photos/230/100" style="padding-bottom: 20px;" />
-              <img src="https://picsum.photos/230/100" />
-            </div>
-            <div class="layout__col--third">
-              <img src="https://picsum.photos/230/100" style="padding-bottom: 20px;" />
-              <img src="https://picsum.photos/230/100" />
+            <div class="layout__col--pacman">
+              <layout>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+              </layout>
+              <layout>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+              </layout>
             </div>
           </layout>
         </div>
       </layout>
     </section>
-    <section>
-      <layout style="width: 100%; background: red;">
-        <img src="https://picsum.photos/1024/760" />
-      </layout>
+
+    <section id="separator-image" style="position: relative; margin-bottom: 80px">
+      <img class="img--fill-container" style="height:460px" src="https://picsum.photos/1024/460" />
+      <p style="color: white; position: relative; top: -460px; left: calc(20px + 26vw); width: 20vw;">We’ve also given quite a bit of thought to the design of our shared workspace.</p>
     </section>
-    <section style="margin-top: 100px">
+
+    <section id="2">
       <layout>
-         <div class="layout__col--quarter">
+        <div class="layout__col--quarter marginned--horizontally">
           <h1 class="number">02</h1>
         </div>
-        <div class="layout__col--triple" style="margin-right: 0">
-          <layout vertical="true" style="margin-bottom: 20px">
-            <div class="layout__col--whole" style="width: 100%">
+        <div class="layout__col--pacman marginned--horizontally">
+          <layout id="intiate" vertical="true" class="marginned--vertically--6x">
+            <div class="layout__col--whole marginned--vertically">
               <layout>
-                <div class="layout__col--triple" style="margin-left: 0; margin-right: 20px">
-                  <img src="https://picsum.photos/480/100" style="height: 100px;" />
+                <div class="layout__col--pacman">
+                  <img class="img--fill-container" src="https://picsum.photos/480/100" />
                 </div>
-                <div class="layout__col--quarter" style="margin-right: 0">
-                  <img src="https://picsum.photos/250/100" style="height: 100px;" />
+                <div class="layout__col--quarter" style="margin-left: 20px">
+                  <img class="img--fill-container" style="width: calc(100% + 20px)" src="https://picsum.photos/250/100" />
                 </div>
               </layout>
-              <layout style="margin-top: 20px">  
-                <div class="layout__col--quarter" style="width: 25%">
+            </div>
+            <div class="layout__col--whole">
+              <layout>
+                <div class="layout__col--quarter" style="margin-right: 20px">
                   <p>Where our designs slowly transform into reality. We see it as a highly iterative, dynamic, catalogued, and collaborative process, where everyone learns from everyone else.</p>
                 </div>
-                <div class="layout__col--triple" style="width: 100%; margin-right: 0">
-                    <img src="https://picsum.photos/500/100" />
+                <div class="layout__col--pacman">
+                  <img class="img--fill-container" style="width: calc(100% + 20px)" src="https://picsum.photos/500/100" />
                 </div>
               </layout>
             </div>
           </layout>
-          <layout vertical="true">
-            <div class="layout__col--whole" style="width: 50%">
+          <layout id="refine" vertical="true" class="marginned--vertically--6x">
+            <div class="layout__col--pacman" style="width: 50vw">
               <p>We strongly believe that there’s always room for our development processes to improve. Always.</p>
             </div>
             <div class="layout__col--whole">
-                <img src="https://picsum.photos/500/100" style="margin-right: 0" />
+              <img class="img--fill-container" style="width: calc(100% + 20px)" src="https://picsum.photos/760/100" />
             </div>
-            <div class="layout__col--whole" style="width: 50%">
+            <div class="layout__col--pacman" style="width: 50vw">
               <p>We’re presently using scrum to manage our projects (or at least our own hacked version of it ;). Courtney keeps everyone in line.</p>
+            </div>
+          </layout>
+          <layout id="collaborate" class="marginned--vertically">
+            <div class="layout__col--quarter">
+              <p>Finally, we think of “develop” not just in a software sense but also in a personal development sense. The mission of squared labs is very much one of learning where team members can develop skills that we hope will serve them well beyond UConn.</p>
+            </div>
+            <div class="layout__col--pacman">
+              <layout>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+              </layout>
+              <layout>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+                <div class="layout__col--half padded">
+                  <img class="img--fill-container" src="https://picsum.photos/231/100">
+                </div>
+              </layout>
             </div>
           </layout>
         </div>
@@ -149,9 +162,6 @@
 
 <script>
 import Construction from "../components/Construction.vue";
-import QuadLayout from "../components/QuadLayout.vue";
-import TripleLayout from "../components/TripleLayout.vue";
-import DualLayout from "../components/DualLayout.vue";
 import Layout from "../components/Layout.vue";
 import ProjectCard from "../components/ProjectCard.vue";
 import MiniPersonCard from "../components/MiniPersonCard.vue";
@@ -159,9 +169,6 @@ import MiniPersonCard from "../components/MiniPersonCard.vue";
 export default {
   components: {
     Construction,
-    QuadLayout,
-    TripleLayout,
-    DualLayout,
     Layout,
     ProjectCard,
     MiniPersonCard
@@ -195,7 +202,12 @@ export default {
   margin: 0;
 }
 
-.layout img {
+.img--fill-container {
   width: 100%;
+  height: 100%;
+}
+
+.marginned--vertically--6x {
+    margin-bottom: 120px;
 }
 </style>
