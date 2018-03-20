98d58ceca96fbea946272ae82dd4a92d392f2de3
Colin McNeil <colinmcneil@gmail.com>
2018-03-20 17:36:57 -0400

    Create Forward & Back Nav Button
    Forward and back button component works and is on the connect page.

--- src/pages/Connect.vue
+++ src/pages/Connect.vue
@@ -55,8 +55,15 @@
         padding-left: 10vw;
         font-family: "Moderat";
     }
+    #back{
+        position: absolute;
+        left: 10px;
+        top: 50%;
+        width:10px;
+        height: 10px;
+        background-color: $dodger-blue;
+    }
     @media only screen and (max-width: 750px) {
-
         #Header-1{
             font-size: 35px;
         }
