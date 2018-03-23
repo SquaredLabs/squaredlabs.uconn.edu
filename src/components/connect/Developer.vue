d5bce630a2a2245899af86fe3330428046e74146
Colin McNeil <colinmcneil@gmail.com>
2018-03-23 13:54:04 -0400

    Create selector
    Create selector for different connect views

---
+++ src/components/connect/Developer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="container">
+        Developer Placeholder
+        <div v-on:click="back">Back</div>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        props: ["back_to_select"],
+        data:function(){
+            return{
+            }
+        },
+        methods:{
+            back(){
+                this.back_to_select();
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+  #container{
+
+  }
+</style>
