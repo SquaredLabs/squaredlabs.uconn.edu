69720141d3d475de4f7e809ead272160471587d9
Brandon Cheng <gluxon@users.noreply.github.com>
2018-05-10 21:40:06 -0400

    SW-46 Replace == with ===

--- src/middleware/checkTheme.js
+++ src/middleware/checkTheme.js
@@ -1,6 +1,6 @@
 export default function(context) {
   let path = context.route.path
-  if (path == "/labs") {
+  if (path === "/labs") {
     context.store.commit("setBlue")
   } else {
     context.store.commit("setWhite")
