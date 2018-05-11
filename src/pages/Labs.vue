a14407a7bd6cf5bae88424efdae1f89b8d403b5e
Brandon Cheng <gluxon@users.noreply.github.com>
2018-05-10 21:45:30 -0400

    SW-46 Contain <p> with a <div> on Labs.vue
    Containing <p> inside a <p> doesn't make sense semantically. There's no
    such thing as a paragraph within a paragraph in English.
    
    Prettier autofix was confused on how to fix this indentation-wise. I
    believe that was because it doesn't support nested paragraphs for the
    reason above.

--- src/pages/Labs.vue
+++ src/pages/Labs.vue
@@ -70,96 +70,98 @@
       id="philosphy"
       class="about">
       <h1 class="aboutTitle">our philosphy</h1>
-      <p class="aboutText"/><p>
-        Students are the lifeblood of any University.
-        When they graduate, many will go on to transform the world
-        — literally. Yet, academia, with its long-standing obsession
-        with status, title, and seniority, too often makes students
-        feel unready or incapable of making a difference until they
-        have a diploma in their hand
-        (maybe it’s their way of justifying their existence in
-        the face of people like Steve Jobs, Bill Gates,
-        Mark Zuckerberg, etc. ;)
-      </p>
-      <p>
-        Anyway, we believe that’s bogus.
-      </p>
-      <p>
-        At UConn we have some of the brightest,
-        most motivated, and hard-working students in the country.
-        At squared labs we believe that students need to be
-        supported to unleash their creativity
-        (and paid well to do it).
-      </p>
-      </p>
+      <div>
+        <p>
+          Students are the lifeblood of any University.
+          When they graduate, many will go on to transform the world
+          — literally. Yet, academia, with its long-standing obsession
+          with status, title, and seniority, too often makes students
+          feel unready or incapable of making a difference until they
+          have a diploma in their hand
+          (maybe it’s their way of justifying their existence in
+          the face of people like Steve Jobs, Bill Gates,
+          Mark Zuckerberg, etc. ;)
+        </p>
+        <p>
+          Anyway, we believe that’s bogus.
+        </p>
+        <p>
+          At UConn we have some of the brightest,
+          most motivated, and hard-working students in the country.
+          At squared labs we believe that students need to be
+          supported to unleash their creativity
+          (and paid well to do it).
+        </p>
+      </div>
     </div>
     <div
       id="who"
       class="about">
       <h1 class="aboutTitle">who we are</h1>
-      <p class="aboutText"/><p>
-        We’re made up mainly of student developers, designers,
-        and content specialists. For a complete listing of current
-        (and past) squared labs team members see our people page.</p>
-      <p>If you want to do what we do, then join us!</p>
-      </p>
+      <div>
+        <p>
+          We’re made up mainly of student developers, designers,
+          and content specialists. For a complete listing of current
+          (and past) squared labs team members see our people page.
+        </p>
+        <p>If you want to do what we do, then join us!</p>
+      </div>
     </div>
     <div
       id="what"
       class="about">
       <h1 class="aboutTitle">what we build</h1>
-      <p class="aboutText"/><p>
-        The sites we’ve built range from the purely informational
-        (e.g., tip.uconn.edu) to the functional (e.g.,
-        lincus.uconn.edu) to a hybrid of the two
-        (e.g., ip.uconn.edu). They all have a common thread —
-        our websites support research and researchers at UConn.
-      </p>
-      <p>
-        If you want to know everything we’ve worked on, and are
-        working on, check out our [projects] page.
-      </p>
-
-
-      </p>
+      <div>
+        <p>
+          The sites we’ve built range from the purely informational
+          (e.g., tip.uconn.edu) to the functional (e.g.,
+          lincus.uconn.edu) to a hybrid of the two
+          (e.g., ip.uconn.edu). They all have a common thread —
+          our websites support research and researchers at UConn.
+        </p>
+        <p>
+          If you want to know everything we’ve worked on, and are
+          working on, check out our [projects] page.
+        </p>
+      </div>
     </div>
     <div
       id="where"
       class="about">
       <h1 class="aboutTitle">where we live</h1>
-      <p class="aboutText">
+      <div>
         Organizationally:
-      </p><p>
-        We’re a subunit within the Center for Open Research
-        Resources & Equipment (also known as COR²E), which is a
-        unit within the Office of the Vice President for Research
-        (or the OVPR).</p>
-
-      Physically:
-      <p>
-        We’re in room G01 of the Biology/Physics
-        building — the mysterious locked room that says
-        [squared labs] on the front and looks like some sort of
-        company (we’re not).
-      </p>
-      </p>
+        <p>
+          We’re a subunit within the Center for Open Research
+          Resources & Equipment (also known as COR²E), which is a
+          unit within the Office of the Vice President for Research
+          (or the OVPR).
+        </p>
+        Physically:
+        <p>
+          We’re in room G01 of the Biology/Physics
+          building — the mysterious locked room that says
+          [squared labs] on the front and looks like some sort of
+          company (we’re not).
+        </p>
+      </div>
     </div>
     <div
       id="space"
       class="about">
       <h1 class="aboutTitle"/>
-      <p class="aboutText"/><p>
-        We take our work environment seriously, because after all,
-        working somewhere that you don’t like is really not all
-        that fun.
-      </p>
-
-      <p>
-        The space we’re currently in was formerly an unused
-        conference room that was being used as an office for one
-        person and storage for furniture and junk.
-      </p>
-      </p>
+      <div>
+        <p>
+          We take our work environment seriously, because after all,
+          working somewhere that you don’t like is really not all
+          that fun.
+        </p>
+        <p>
+          The space we’re currently in was formerly an unused
+          conference room that was being used as an office for one
+          person and storage for furniture and junk.
+        </p>
+      </div>
     </div>
   </div>
 </template>
