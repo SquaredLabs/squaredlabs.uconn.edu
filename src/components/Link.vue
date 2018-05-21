bbf3e884ed0c61c1196e6aeca761f15f092811d3
Colin McNeil <colinmcneil@gmail.com>
2018-05-21 12:57:14 -0400

    Fix code styling issues

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -69,7 +69,7 @@ export default {
   padding-right: 2px;
   transition: background 0.15s, padding 0.05s;
   text-decoration: none;
-  font-weight:normal;
+  font-weight: normal;
 }
 .button:hover {
   background-color: #6aa1f4;
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -170,7 +170,7 @@ export default {
     PersonCard,
     SLink
   },
-  data: ()=>({
+  data: () => ({
     people: [],
     projects: []
   })
