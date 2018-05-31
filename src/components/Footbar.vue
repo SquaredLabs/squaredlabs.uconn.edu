19bf85919c157c958592ac6767ff9358abc71575
alecmakeus <alecmarcus.n@gmail.com>
2018-05-31 14:40:38 -0400

    Closes #62 #63, fix jumbo title typeface

--- src/components/Footbar.vue
+++ src/components/Footbar.vue
@@ -4,7 +4,7 @@
     class="footbar">
     <SLink
       href="https://uconn.edu">
-      © 2018 UConn
+      ©2018 UConn
     </SLink>
     <SLink
       href="https://core.uconn.edu">
@@ -19,13 +19,13 @@
     <SLink>
       Link 2
     </SLink>
-    <figure class="footbar__brand">
+    <figure class="footbarBrand">
       <img src="~/assets/images/wordmark.png" >
     </figure>
-    <span class="footbar__signature">
+    <span class="footbarSignature">
       Designed & developed by
       <br >
-      <span class="footbar__signature">[squared labs]</span>
+      <span class="footbarSignature-mono">[squared labs]</span>
       <br >
       at UCONN
     </span>
@@ -49,6 +49,7 @@ export default {
   filter: brightness(0) invert(1);
 }
 .footbar {
+  font-family: SpaceMono;
   padding: 20px;
   text-align: center;
   display: flex;
@@ -60,27 +61,27 @@ export default {
   z-index: 2;
 }
 
-.footbar__brand {
+.footbarBrand {
   flex-basis: calc(100% / 8 * 2);
   flex-grow: 0;
   min-width: 0;
   margin: 0;
 }
 
-.footbar__brand > img {
+.footbarBrand > img {
   max-width: 60%;
 }
 
-.footbar__signature {
+.footbarSignature {
   flex-basis: 100%;
-  font-family: "SpaceMono";
+  font-family: "Moderat";
   padding-top: 23px;
   color: $dodger-blue;
   line-height: 1.5;
 }
 
-.footbar__signature-bold {
+.footbarSignature-mono {
   font-size: 26px;
-  font-weight: bold;
+  font-family: "SpaceMono";
 }
 </style>
