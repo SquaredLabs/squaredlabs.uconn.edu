25be088423a3001b954f44af50eff6409e1cb7cb
alecmakeus <alecmarcus.n@gmail.com>
2018-06-04 16:35:41 -0400

    Implement basic layout for people page. Closes #109.

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -46,11 +46,11 @@ export default {
   },
   methods: {
     expand() {
-      this.$store.commit("selectProject", this.project.id)
-      if (this.$route.path !== '/projects') {
-        this.$router.push('projects')
-      }
       document.querySelector("html").classList.add("noScroll")
+      this.$store.commit("selectProject", this.project.id)
+      if (this.$route.path !== "/projects") {
+        this.$router.push("projects")
+      }
     }
   }
 }
