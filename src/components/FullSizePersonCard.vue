cd6650c4ab3b8149d2019875061078a048514337
alecmakeus <alecmarcus.n@gmail.com>
2018-06-04 23:06:36 -0400

    Implement visual responsive layout for people page. Closes #98.

--- src/components/FullSizePersonCard.vue
+++ src/components/FullSizePersonCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fullSizePersonCard">
     <div
-      :style="`background: url('${background}') center/150% no-repeat`"
+      :style="`background-image: url('${background}')`"
       class="personPhoto" />
     <div class="personContent">
       <div class="personPreview">
@@ -26,41 +26,32 @@ export default {
 <style lang="scss" scoped>
 @import "~assets/styles/vars";
 
-@media screen and (min-width: tablet) {
-}
-
-.fullSizePersonCard {
-  transition: all 0.25s ease;
-  // position: relative;
-  height: 14.552em;
-  // margin-bottom: 40px;
-
-  &:hover .personContent {
-    // height: 11.642em;
-    // margin: -14.642em 0 0;
-    // transition: all 0.25s ease, margin 0.25s ease .05s;
-
-    .personAbout {
-      // margin: 0 1.25em;
-      // opacity: 1;
-      // transition: opacity .15s ease .15s;
+@media screen and (max-width: $tablet) {
+  div.fullSizePersonCard:nth-child(2) {
+    grid-column: 1 / 5;
+    margin-left: -20px;
+    width: calc(100% + 40px);
+    height: 50vh;
+  }
+  div.fullSizePersonCard:nth-child(2) ~ .fullSizePersonCard {
+    height: calc((100vw - 120px) / 4);
+    .personPhoto {
+      width: 100%;
+    }
+    .personContent {
+      width: 0;
+      height: 0;
+      display: none;
     }
   }
-
-  &:hover .personPhoto {
-    // margin: 30px;
-    // filter: brightness(60%);
-  }
-}
-
-@media screen and (max-width: $tablet) {
 }
 
 .personPhoto {
   height: 100%;
-  width: calc(50% - 10px);
   transition: all 0.25s ease;
   display: inline-block;
+  background-size: cover;
+  background-position: center;
 }
 
 .personContent {
@@ -92,7 +83,71 @@ export default {
   font-size: 1em;
 }
 
-.personAbout {
-  // font-size: 0.8em;
+@media screen and (min-width: $tablet) {
+  div.fullSizePersonCard {
+    transition: all 0.25s ease;
+    height: 22.737em;
+    grid-column-start: 2;
+
+    &:nth-child(2) {
+      margin-bottom: 40px;
+      .personPhoto {
+        width: calc(50% - 10px);
+      }
+      .personContent {
+        margin-bottom: -40px;
+      }
+    }
+    &:not(:nth-child(2)) {
+      grid-column: span 1;
+      width: calc(50% - 10px);
+      height: calc((100vw - 300px) / 12);
+      .personContent {
+        display: none;
+        width: 0;
+        height: 0;
+      }
+      .personPhoto {
+        width: 100%;
+      }
+    }
+    &:nth-child(3) {
+      height: 22.737em;
+      grid-column: 1 / span 1;
+      grid-row-start: 2;
+      width: 200%;
+      margin-left: -100%;
+      margin-top: 20px;
+    }
+    &:nth-child(4) {
+      height: 22.737em;
+      grid-column: 6 / span 1;
+      grid-row-start: 2;
+      width: 200%;
+      margin-top: 20px;
+    }
+  }
+}
+
+@media screen and (min-width: $desktop) {
+  div.fullSizePersonCard {
+    &:nth-child(2) {
+      grid-column-start: 3;
+    }
+    &:not(:nth-child(2)) {
+      height: calc((100vw - 300px) / 16);
+    }
+    &:nth-child(3) {
+      height: 22.737em;
+      width: calc(100% + 40px);
+      grid-column: 1 / span 2;
+      margin-left: -40px;
+    }
+    &:nth-child(4) {
+      height: 22.737em;
+      width: calc(100% + 40px);
+      grid-column: 7 / span 2;
+    }
+  }
 }
 </style>
