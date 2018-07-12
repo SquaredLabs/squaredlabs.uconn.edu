3e7772ded3172d840e43e6165968ca249a07f1b2
alecmakeus <alecmarcus.n@gmail.com>
2018-07-12 16:58:36 -0400

    Closes #123, disables aims section, add grid to lab pg

--- src/pages/labs.vue
+++ src/pages/labs.vue
@@ -1,8 +1,8 @@
 <template>
-  <Grid>
+  <Grid class="noMarginTop">
     <div
       id="titleTextContainer"
-      class="col-sm-3 col-md-5 col-7 col-lg-9">
+      class="col-sm-4 col-md-6 col-8 col-lg-12">
       <div id="titleText">
         <TitleLine title-positioning="design">
           <template slot="title">
@@ -10,7 +10,7 @@
           </template>
           <template slot="hoverContent">
             <p>
-              <span class="weAre">you wouldn't build</span>
+              <span class="weAre">you wouldn&rsquo;t build</span>
               a building without architectural drawings.  We don’t build websites without
               spending a considerable amount of time on design, ensuring that every choice contributes to crafting
               the most effective solution.
@@ -69,7 +69,7 @@
               until it functions, breaks, and takes its own place in the world.
             </p>
             <p>
-              <span class="weAre">"create" is about</span>
+              <span class="weAre">&ldquo;create&rdquo; is about</span>
               the reward you get when your site goes live, and your audience
               gets the opportunity to experience your hard work first hand. The feeling you get having
               built something from nothing — hopefully leaving the internet ever so slightly more useful or
@@ -79,39 +79,13 @@
         </TitleLine>
       </div>
     </div>
-    <div
-      id="plusIcons"
-      class="col-sm-1">
-      <div class="plus">
-        +<br>
-        +
-      </div>
-    </div>
-    <div id="aims">
-      <div id="leftContent">
-        <div id="text">
-          <p>
-            <span class="weAre">we are squared</span>
-            labs: a group of undergratuate students
-            at the University of Connecticut. We support research at
-            the University and bring the sciences and arts closer
-            together.
-          </p>
-          <p>
-            <span class="weAre">we are collaborators:</span>
-            programmers, designers, illustrators,
-            photographers, journalists, and scientists.
-          </p>
-          <p>
-            <span class="weAre">we create and </span>
-            build tools, websites, identities, art,
-            and more. Really, the only thing we don’t do is say no.
-          </p>
-        </div>
+
+    <!-- <div
+      id="aims"
+      class="col-sm-4 col-md-5">
         <div class="aimL">
-          <div class="aimI5">
-            To leave UConn better than we found it.
-          </div>
+        <div class="aimI5">
+          To leave UConn better than we found it.
         </div>
       </div>
       <div id="rightContent">
@@ -143,41 +117,56 @@
           </div>
         </div>
       </div>
+    </div> -->
+    <div
+      id="aboutSummary"
+      class="aboutModule col-sm-4 col-md-5">
+      <div>
+        <p>
+          <span class="weAre">we are squared labs:</span> a group of undergratuate students at the University of Connecticut. We support research at the University and bring the sciences and arts closer together.
+        </p>
+        <p>
+          <span class="weAre">we are collaborators:</span> programmers, designers, illustrators, photographers, journalists, and scientists.
+        </p>
+        <p>
+          <span class="weAre">we create and build</span> tools, websites, identities, art, and more. Really, the only thing we don’t do is say no.
+        </p>
+      </div>
     </div>
-    <div class="aboutText col-sm-4 col-md-5">
-      <div
-        id="philosphy"
-        class="about">
-        <h1 class="aboutTitle">our philosphy</h1>
-        <div>
-          <p>
-            Students are the lifeblood of any University. When they graduate, many will go on to transform the world&mdash;literally. Yet, academia, with its long-standing obsession with status, title, and seniority, too often makes students feel unready or incapable of making a difference until they have a diploma in their hand (maybe it’s their way of justifying their existence in the face of people like Steve Jobs, Bill Gates, Mark Zuckerberg, etc. ;)
-          </p>
-          <p>
-            Anyway, we believe that’s bogus.
-          </p>
-          <p>
-            At UConn we have some of the brightest, most motivated, and hard-working students in the country. At squared labs we believe that students need to be supported to unleash their creativity (and paid well to do it).
-          </p>
-        </div>
+    <div
+      id="philosphy"
+      class="aboutModule col-sm-4 col-md-5">
+      <h1 class="aboutTitle">our philosphy</h1>
+      <div>
+        <p>
+          Students are the lifeblood of any University. When they graduate, many will go on to transform the world&mdash;literally. Yet, academia, with its long-standing obsession with status, title, and seniority, too often makes students feel unready or incapable of making a difference until they have a diploma in their hand (maybe it’s their way of justifying their existence in the face of people like Steve Jobs, Bill Gates, Mark Zuckerberg, etc. ;)
+        </p>
+        <p>
+          Anyway, we believe that’s bogus.
+        </p>
+        <p>
+          At UConn we have some of the brightest, most motivated, and hard-working students in the country. At squared labs we believe that students need to be supported to unleash their creativity (and paid well to do it).
+        </p>
       </div>
     </div>
     <div
       id="who"
-      class="about">
-      <h1 class="aboutTitle">who we are</h1>
-      <div>
-        <p>
-          We’re made up mainly of student developers, designers,
-          and content specialists. For a complete listing of current
-          (and past) squared labs team members see our people page.
-        </p>
-        <p>If you want to do what we do, then join us!</p>
-      </div>
+      class="aboutModule col-sm-4 col-md-5">
+      <h1 class="aboutTitle">
+        who we are
+      </h1>
+      <p>
+        We’re made up mainly of student developers, designers,
+        and content specialists. For a complete listing of current
+        (and past) squared labs team members see our people page.
+      </p>
+      <p>
+        If you want to do what we do, then join us!
+      </p>
     </div>
     <div
       id="what"
-      class="about">
+      class="aboutModule col-sm-4 col-md-5">
       <h1 class="aboutTitle">what we build</h1>
       <div>
         <p>
@@ -189,36 +178,33 @@
         </p>
         <p>
           If you want to know everything we’ve worked on, and are
-          working on, check out our [projects] page.
+          working on, check out our <SLink href="/projects">projects</SLink> page.
         </p>
       </div>
     </div>
     <div
       id="where"
-      class="about">
+      class="aboutModule col-sm-4 col-md-5">
       <h1 class="aboutTitle">where we live</h1>
       <div>
-        Organizationally:
-        <p>
-          We’re a subunit within the Center for Open Research
-          Resources &amp; Equipment (also known as COR²E), which is a
-          unit within the Office of the Vice President for Research
-          (or the OVPR).
-        </p>
-        Physically:
-        <p>
-          We’re in room G01 of the Biology/Physics
-          building — the mysterious locked room that says
-          [squared labs] on the front and looks like some sort of
-          company (we’re not).
-        </p>
-      </div>
-    </div>
-    <div
-      id="space"
-      class="about">
-      <h1 class="aboutTitle"/>
-      <div>
+        <div class="subsection">
+          <h2 class="subheading">Organizationally:</h2>
+          <p>
+            We’re a subunit within the Center for Open Research
+            Resources &amp; Equipment (also known as COR²E), which is a
+            unit within the Office of the Vice President for Research
+            (or the OVPR).
+          </p>
+        </div>
+        <div class="subsection">
+          <h2 class="subheading">Physically:</h2>
+          <p>
+            We’re in room G01 of the Biology/Physics
+            building — the mysterious locked room that says
+            <span class="stylizedName">[squared labs]</span> on the front and looks like some sort of
+            company (we’re not).
+          </p>
+        </div>
         <p>
           We take our work environment seriously, because after all,
           working somewhere that you don’t like is really not all
@@ -237,197 +223,197 @@
 <script>
 import SLink from "../components/Link"
 import TitleLine from "../components/lab/TitleLine"
+import Grid from "../components/GridLayout"
 
 export default {
   components: {
     SLink,
-    TitleLine
+    TitleLine,
+    Grid
   }
 }
 </script>
 
 <style lang="scss">
 @import "~assets/styles/vars";
+@import "~assets/styles/grid";
 
-#titleTextContainer {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 2em;
-}
-#titleText {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-#plusIcons {
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  font-size: 10em;
-  top: 0.5em;
-  right: 5%;
-  color: white;
-}
-.plug {
-  margin-top: 0;
-}
-
-#aims {
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-}
-#aims #leftContent {
-  flex: 0 0 40%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-}
-#text {
-  flex: 0 0 40%;
-  color: white;
-  padding: 0 2em 4em 2em;
-}
-.weAre {
+// #aims {
+//   display: flex;
+//   justify-content: space-between;
+//   flex-wrap: wrap;
+// }
+// #aims #leftContent {
+//   flex: 0 0 40%;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-end;
+// }
+.weAre,
+.aboutTitle {
   font-family: "SpaceMono";
   font-variant: small-caps;
+  font-weight: bold;
 }
-#aims #rightContent {
-  flex: 0 0 60%;
-  display: flex;
-  flex-direction: column;
+// #aims #rightContent {
+//   flex: 0 0 60%;
+//   display: flex;
+//   flex-direction: column;
+// }
+// #rightContent #title {
+//   font-size: 3em;
+//   text-align: center;
+//   color: white;
+//   font-family: "SpaceMono";
+//   margin-top: 0;
+//   font-weight: 500;
+//   line-height: 1em;
+// }
+// #aimboxes {
+//   flex-basis: 100%;
+//   position: relative;
+//   width: 100%;
+// }
+// #rightContent #aimBoxes {
+//   display: flex;
+//   flex-wrap: wrap;
+//   justify-content: space-around;
+// }
+// #aimBoxes .aim {
+//   position: relative;
+//   flex: 0 0 15em;
+//   height: 15em;
+// }
+// .aim .aimI {
+//   background-color: white;
+//   position: absolute;
+//   width: 80%;
+//   height: 80%;
+// }
+// .aim .aimI1 {
+//   top: 0;
+// }
+// .aim .aimI2 {
+//   bottom: 0;
+// }
+// .aim .aimI3 {
+//   top: 0;
+//   right: 0;
+// }
+// .aim .aimI4 {
+//   bottom: 0;
+//   right: 0;
+// }
+// .aimL {
+//   flex: 0 0 15em;
+//   width: 15em;
+//   right: 0;
+// }
+// .aimI5 {
+//   background-color: white;
+//   /*position: relative;*/
+//   width: 80%;
+//   height: 80%;
+//   text-align: center;
+//   box-sizing: border-box;
+//   padding: 20px;
+// }
+// .aimI {
+//   text-align: center;
+//   box-sizing: border-box;
+//   padding: 20px;
+// }
+#aboutSummary {
+  font-size: 1.953em;
+  margin: 4.768em 0;
 }
-#rightContent #title {
-  font-size: 3em;
-  text-align: center;
+.aboutModule {
   color: white;
-  font-family: "SpaceMono";
-  margin-top: 0;
-  font-weight: 500;
-  line-height: 1em;
-}
-#aimboxes {
-  flex-basis: 100%;
-  position: relative;
-  width: 100%;
-}
-#rightContent #aimBoxes {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-}
-#aimBoxes .aim {
-  position: relative;
-  flex: 0 0 15em;
-  height: 15em;
-}
-.aim .aimI {
-  background-color: white;
-  position: absolute;
-  width: 80%;
-  height: 80%;
-}
-.aim .aimI1 {
-  top: 0;
-}
-.aim .aimI2 {
-  bottom: 0;
-}
-.aim .aimI3 {
-  top: 0;
-  right: 0;
-}
-.aim .aimI4 {
-  bottom: 0;
-  right: 0;
-}
-.aimL {
-  flex: 0 0 15em;
-  width: 15em;
-  right: 0;
-}
-.aimI5 {
-  background-color: white;
-  /*position: relative;*/
-  width: 80%;
-  height: 80%;
-  text-align: center;
-  box-sizing: border-box;
-  padding: 20px;
-}
-.aimI {
-  text-align: center;
-  box-sizing: border-box;
-  padding: 20px;
-}
-.about {
-  width: 60%;
-  color: white;
-  padding-left: 2em;
 }
 .aboutTitle {
-  font-size: 4em;
-  font-weight: 500;
+  margin: 0.25em 0;
+  font-size: 3.052em;
   line-height: 1em;
   font-family: "SpaceMono";
+}
+.subheading {
+  font-size: 1.563em;
+  margin: 0.25em 0 0 0;
+}
+.subsection {
+  width: 48%;
+  display: inline-block;
+}
+.subsection ~ .subsection {
+  margin-left: 1.25em;
+}
+.subsection p {
+  margin-top: 0.25em;
+}
+.stylizedName {
+  font-family: "Space Mono";
+}
+//Desktop Only
+@media screen and (min-width: $desktop) {
+  section.gridded.noMarginTop {
+    margin-top: -10vh;
+  }
 }
 // Tablet
 @media (min-width: $tablet) and (max-width: $desktop) {
-  #aims #leftContent {
-    flex: 0 0 100%;
-  }
-  #aims #rightContent {
-    flex: 0 0 100%;
-    top: -260px;
-    position: relative;
-  }
+  // #aims #leftContent {
+  //   flex: 0 0 100%;
+  // }
+  // #aims #rightContent {
+  //   flex: 0 0 100%;
+  //   top: -260px;
+  //   position: relative;
+  // }
   #plusIcons {
     font-size: 10em;
     top: 1.5em;
   }
   .about {
-    width: calc(100% - 4em);
+    // width: calc(100% - 4em);
     color: white;
-    padding: 0 2em 0 2em;
+    // padding: 0 2em 0 2em;
   }
-  .aim {
-    margin: 10px;
-  }
-  .aimL {
-    top: 300px;
-    position: relative;
-  }
+  // .aim {
+  //   margin: 10px;
+  // }
+  // .aimL {
+  //   top: 300px;
+  //   position: relative;
+  // }
 }
 @media (min-width: 0px) and (max-width: $tablet) {
-  #aims #leftContent {
-    flex: 0 0 100%;
-  }
-  #aims #rightContent {
-    flex: 0 0 100%;
-  }
-
+  // #aims #leftContent {
+  //   flex: 0 0 100%;
+  // }
+  // #aims #rightContent {
+  //   flex: 0 0 100%;
+  // }
   #plusIcons {
     font-size: 5em;
     top: 6.5em;
     right: 10%;
   }
   .about {
-    width: calc(100% - 4em);
+    // width: calc(100% - 4em);
     color: white;
-    padding: 0 2em 0 2em;
+    // padding: 0 2em 0 2em;
   }
-  .aim {
-    transform: translateX(20%);
-  }
-  .aimI {
-    top: 0;
-    left: 0;
-    width: 90%;
-    height: 90%;
-  }
-  .aimL {
-    position: relative;
-  }
+  // .aim {
+  //   transform: translateX(20%);
+  // }
+  // .aimI {
+  //   top: 0;
+  //   left: 0;
+  //   width: 90%;
+  //   height: 90%;
+  // }
+  // .aimL {
+  //   position: relative;
+  // }
 }
 </style>
