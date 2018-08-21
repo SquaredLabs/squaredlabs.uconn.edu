788909cf0b93d430390f48f13bd6296a9b0be37b
alecmakeus <alecmarcus.n@gmail.com>
2018-08-21 11:26:59 -0400

    Add correct favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,8 @@ module.exports = {
     middleware: "checkTheme"
   },
   build: {
+    assetsPublicPath: "/",
+    assetsSubDirectory: "static",
     extend(config, { isDev, isClient }) {
       // ...
     }
@@ -22,6 +24,50 @@ module.exports = {
         hid: "description",
         name: "description",
         content: "The team website."
+      },
+      {
+        name: "msapplication-TileColor",
+        content: "#6aa1f4"
+      },
+      {
+        name: "msapplication-config",
+        content: "/static/browserconfig.xml?v=E656kOkMMx"
+      },
+      {
+        name: "theme-color",
+        content: "#6aa1f4"
+      }
+    ],
+    link: [
+      {
+        rel: "shortcut icon",
+        href: "/static/favicon.ico?v=E656kOkMMx"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/static/favicon-16x16.png?v=E656kOkMMx"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/static/favicon-32x32.png?v=E656kOkMMx"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/static/apple-touch-icon.png?v=E656kOkMMx"
+      },
+      {
+        rel: "manifest",
+        href: "/static/site.webmanifest?v=E656kOkMMx"
+      },
+      {
+        rel: "mask-icon",
+        href: "/static/safari-pinned-tab.svg?v=E656kOkMMx",
+        color: "#5bbad5"
       }
     ]
   }
