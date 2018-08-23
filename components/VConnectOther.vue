27cf383db8d342bb3ee1bd23587893b351f2e896
alecmakeus <alecmarcus.n@gmail.com>
2018-08-23 02:47:53 -0400

    Use connect buttons on connect page

--- components/VConnectOther.vue
+++ components/VConnectOther.vue
@@ -4,7 +4,6 @@
       If you randomly (or not so randomly) stumbled upon our website and after reading it felt a deeper
       connection to our mission you should seriously consider a philanthropic contribution.
       Most funds will go to feeding starving undergraduates on the team.
-
       <p><strong>Just think:</strong></p>
       <p class="level">At the $1 level:</p>
       <p class="level-reward">You can buy a team member half a bottle of iced tea from a UConn cafe.</p>
@@ -21,22 +20,28 @@
       <p class="level-reward">You can sponsor a UConn cookie platter for one of our workshops.</p>
       <p class="level">At the $100 level:</p>
       <p class="level-reward">You can give a 5 team members a 50 cent raise (for one week).</p>
-
       <p>In all seriousness, in addition to food, our lab has a variety of needs
       (from white board markers to computer monitors).  Any and all contributions are extremely appreciated 😁.</p>
-      <div id="icons">
-        <div class="icon">
-          <img
-            alt="Donate Bitcoin"
-            src="~/assets/images/connect/btc.svg" >
-        </div>
+      <div class="donateButton">
+        <v-connect-card
+          title="Donate to ⬚² labs"
+          link="http://www.foundation.uconn.edu/send-your-gift/" >
+          <v-connect-button-art-btc />
+        </v-connect-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import VConnectCard from "~/components/VConnectCard.vue"
+import VConnectButtonArtBtc from "~/components/VConnectButtonArtBtc.vue"
+
 export default {
+  components: {
+    VConnectCard,
+    VConnectButtonArtBtc
+  },
   props: {
     back: { type: Function, required: true }
   }
@@ -52,31 +57,6 @@ export default {
 .level-reward {
   margin: 0;
 }
-#icons {
-  padding-top: 40px;
-  display: flex;
-  justify-content: center;
-}
-.icon {
-  width: 230px;
-  height: 100px;
-  background-color: white;
-  display: flex;
-  justify-content: center;
-  transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1);
-}
-.icon img {
-  height: 45px;
-  margin-top: 28px;
-  transition: all 0.2s cubic-bezier(0.4, 0, 0.2, 1);
-}
-.icon:hover {
-  cursor: pointer;
-}
-.icon:hover img {
-  height: 50px;
-  margin-top: 26px;
-}
 #back {
   background-color: rgba(255, 255, 255, 0.5);
   position: absolute;
@@ -84,5 +64,10 @@ export default {
   left: 50%;
   transform: translateX(-50%);
   bottom: 40px;
+}
+.donateButton {
+  display: flex;
+  justify-content: center;
+  padding-top: 40px;
 }
 </style>
