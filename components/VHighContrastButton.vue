6600adae2f946936651536e1a5b7a5bb22136628
alecmakeus <alecmarcus.n@gmail.com>
2018-11-27 13:54:21 -0500

    Create standaloneVHighContrastButton component

---
+++ components/VHighContrastButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    class="high-contrast"
+    @click="$emit('click')">High Contrast Mode</button>
+</template>
+
+<script>
+export default {}
+</script>
+
+
+<style lang="scss" scoped>
+@import "~assets/styles/vars";
+
+.high-contrast {
+  outline: none;
+  border: none;
+  background-color: $solid-blue;
+  color: white;
+  padding: 0.8em;
+  transition: all ease 0.2s;
+  width: 100%;
+  cursor: pointer;
+  font-family: "SpaceMono";
+  font-size: 0.8em;
+  letter-spacing: 0.05em;
+  font-weight: 600;
+  text-transform: uppercase;
+  transition: all 0.15s ease;
+}
+
+.high-contrast:hover {
+  font-size: 1em;
+  padding: 1.25em;
+}
+
+.high-contrast:hover,
+.highContrast .high-contrast {
+  background-color: black;
+  color: white;
+}
+</style>
